7ad6bf3a6e4960664fb30974d25083a8b6fb5d11
tycable <tycable@gmail.com>
2018-10-31 21:30:55 +0800

    第三次
    一点点问题

--- vscripts/catapult_attack_lua.lua
+++ vscripts/catapult_attack_lua.lua
@@ -3,7 +3,6 @@ LinkLuaModifier( "modifier_catapult_attack", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_catapult_attack_absorb", LUA_MODIFIER_MOTION_NONE )
 
 function catapult_attack_lua:OnSpellStart( )
-	self.damage_absorb=self:GetSpecialValueFor("damage_absorb")
 	self.bonus_damage=self:GetSpecialValueFor("bonus_damage")
 	self.duration=self:GetSpecialValueFor("duration")
 
